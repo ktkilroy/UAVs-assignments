428d3a5af0923eb5dda0af8b164e0df382d55f97
ktkilroy <89943375+ktkilroy@users.noreply.github.com>
2021-09-07 23:01:47 -0400

    Add files via upload

--- subscriber.py
+++ subscriber.py
@@ -1,18 +1,43 @@
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+def move():
+    # starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
 
 def callback(data):
     rospy.loginfo("RECIEVED DATA: %s", data.data)
+    rospy.Publisher('/turtle2/cmd_vel', turtle1_move, queue_size=10)
+
 
 def listener():
     rospy.init_node("subscriber_node", anonymous = True)
-    rospy.Subscriber('robot_cleaner', String, callback)
+    turtle1_move = rospy.Subscriber('/turtle1/cmd_vel', String, callback)
     rospy.spin()
-    velocity = Twist()
-    velocity = data.data
 
 if __name__ == '__main__':
     try:
         listener()
     except rospy.ROSInterruptException:
         pass
+
+publisher
+
+
+callback fun name moverturtle2
+have a log here
+want to publish to T2 
+
+
+        listner 
+        sub
+
+        callback
+
+        need to send over /cmd/vel
+        twist
+
+turtle 1 moves with teleop
